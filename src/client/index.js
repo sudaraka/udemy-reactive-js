35b9a4152df28269c0ec10b9d17b67489a05ff57
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-08-06 19:21:00 +0530

    Traditional autocomplete

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,31 @@
-import moment from 'moment'
 import $ from 'jquery'
 
-$('body').text(moment().format())
+const
+  $title = $('#title'),
+  $results = $('#results'),
 
+  getItems = title => {
+    console.log('Querying', title)
+
+    return new Promise(resolve => {
+      window.setTimeout(() => {
+        resolve([ title, 'Item 2', `Another ${Math.random()}` ])
+      }, 500 + (Math.random() * 1000))  // eslint-disable-line no-extra-parens
+    })
+  }
+
+$title.on('keyup', e => {
+  const
+    title = e.target.value
+
+  getItems(title)
+    .then(items => {
+      const
+        $items = items.map(item => $('<li />').text(item))
+
+      $results
+        .empty()
+        .append($items)
+
+    })
+})
